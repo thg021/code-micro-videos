5256fae89c8b98a1d562256b9b1812f9e7ea37a1
Thiago Silva <thiago.silva@hotmail.com>
2021-12-27 15:29:11 -0300

    refactor: add controllers method exception list

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api'], function(){
-    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
-    Route::resource('genres', 'GenreController', ['except' => ['create', 'edit']]);
+    $exceptMethods = [
+        'except' => ['create', 'edit']
+    ]; 
+
+    Route::resource('categories', 'CategoryController', $exceptMethods );
+    Route::resource('genres', 'GenreController', $exceptMethods );
 });
