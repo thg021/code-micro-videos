0796e3474ca0efe2c424c326eae6c251396eb70d
Thiago Silva <thiago.silva@hotmail.com>
2022-01-25 23:22:02 -0300

    feat: resource video
    add controller, model and factory video

---
+++ app/Http/Controllers/Api/VideoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Video;
+use App\Http\Controllers\Api\BasicCrudController;
+
+class VideoController extends BasicCrudController
+{
+   public function __construct()
+   {
+      
+   }
+   
+   protected function model()
+   {
+
+   }
+
+   protected function rulesStore()
+   {
+       
+   }
+
+   protected function rulesUpdate()
+   {
+       
+   }
+}
